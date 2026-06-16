15ee2166dddb82798a91f135ab0fb748c033bc94
korosh020K <koroshjaber69@gmail.com>
2026-06-16 16:52:44 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ if (!localStorage.getItem("welcomeShown")) {
     alert("به سایت من خوش آمدید");
     localStorage.setItem("welcomeShown", "yes");
 }
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("show");
+}
